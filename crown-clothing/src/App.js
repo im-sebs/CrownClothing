785f6bfc101358e5e33aae720435ad6feef8e72b
Sebastian <js.diazr@uniandes.edu.co>
2020-03-29 01:07:50 -0500

    Basic Routing

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,11 +1,35 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const TopicList = () => (
+  <div>
+    <h1>TOPIC LIST PAGE</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>TOPIC DETAIL PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/topics" component={TopicList} />
+        <Route path="/topics/:topicId" component={TopicDetail} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
