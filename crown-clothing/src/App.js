907e01e1b218f447da010fdb6ebe2a19d43649ae
Sebastian <js.diazr@uniandes.edu.co>
2020-05-15 21:12:19 -0500

    Updated Collection Preview Component

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,34 +1,15 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
+import ShopPage from "./pages/shop/ShopPage";
 import { Switch, Route } from "react-router-dom";
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const TopicList = () => (
-  <div>
-    <h1>TOPIC LIST PAGE</h1>
-  </div>
-);
-
-const TopicDetail = () => (
-  <div>
-    <h1>TOPIC DETAIL PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/topics" component={TopicList} />
-        <Route path="/topics/:topicId" component={TopicDetail} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
